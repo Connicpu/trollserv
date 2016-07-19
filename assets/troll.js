fb614684a712b05e6ce12f261d0f79f9c3685667
Connor Hilarides <connorcpu@live.com>
2016-07-19 10:59:40 -0700

    Started work

---
+++ assets/troll.js
@@ -0,0 +1,14 @@
+function onReady() {
+    var width = window.innerWidth || window.clientWidth;
+    var height = window.innerHeight || window.clientHeight;
+
+    var trollman = document.getElementById("trollman");
+
+    if (width > height) {
+        
+    } else {
+
+    }
+}
+
+document.addEventListener("DOMContentLoaded", onReady);
